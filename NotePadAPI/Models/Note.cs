78ec5ba38c7e76662ed82398eae9a48300e44865
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-08-29 10:57:46 +0600

    Add Note model

---
+++ NotePadAPI/Models/Note.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NotePadAPI.Models
+{
+    public class Note
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastUpdatedAt { get; set; }
+    }
+}
