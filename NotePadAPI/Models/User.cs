0d9f40897dfe3be20ee4d9df47efed0226de530b
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-08-29 10:54:45 +0600

    Add User model

---
+++ NotePadAPI/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NotePadAPI.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(100)]
+        public string Name { get; set; } = null!;
+        [MaxLength(100)]
+        public string Email { get; set; } = null!;
+        public string PasswordHash { get; set; } = null!;
+        public byte[] PasswordSalt { get; set; } = null!;
+        
+    }
+}
