e098f4b64c5f5b639b1696382509da92cdef1f02
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-08-30 08:56:57 +0600

    Add UserRegistrationDto

---
+++ NotePadAPI/Models/DTO/UserRegistrationDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NotePadAPI.Models.DTO
+{
+    public class UserRegistrationDto
+    {
+        [MaxLength(100)]
+        public string Name { get; set; } = null!;
+        [MaxLength(100)]
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
