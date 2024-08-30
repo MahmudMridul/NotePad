5aa13bb155ecf6698d01a0557f9cbbaf24b622cf
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-08-30 10:06:10 +0600

    Add GenerateSalt, HashPassword, GetSaltAndHash functions

--- NotePadAPI/Utils/UserUtils.cs
+++ NotePadAPI/Utils/UserUtils.cs
@@ -1,11 +1,13 @@
 ﻿using System.Net.Mail;
+using System.Security.Cryptography;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace NotePadAPI.Utils
 {
     internal class UserUtils
     {
-        internal static bool ValidEmail(string email)
+        internal static bool IsValidEmail(string email)
         {
             if(string.IsNullOrEmpty(email)) return false;
             if(string.IsNullOrWhiteSpace(email)) return false;
@@ -35,6 +37,56 @@ namespace NotePadAPI.Utils
             string pattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,}$";
             Regex regex = new Regex(pattern);
             return regex.IsMatch(password);
+        }
+
+        internal static object GetSaltAndHash(string password)
+        {
+            byte[] salt = GenerateSalt();
+            string hash = HashPassword(password, salt);
+            return new { salt, hash };
+        }
+
+        internal static byte[] GenerateSalt(int size = 16)
+        {
+            byte[] salt = new byte[size];
+            try
+            {
+                RandomNumberGenerator.Fill(salt);
+                return salt;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return salt;
+        }
+
+        internal static string HashPassword(string password, byte[] salt)
+        {
+            try
+            {
+                using (SHA256 sha256 = SHA256.Create())
+                {
+                    byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+                    byte[] saltedPassword = new byte[salt.Length + passwordBytes.Length];
+
+                    Buffer.BlockCopy(passwordBytes, 0, saltedPassword, 0, passwordBytes.Length);
+                    Buffer.BlockCopy(salt, 0, saltedPassword, passwordBytes.Length, salt.Length);
+
+                    byte[] hashedBytes = sha256.ComputeHash(saltedPassword);
+
+                    byte[] hashedPassWithSalt = new byte[hashedBytes.Length + salt.Length];
+                    Buffer.BlockCopy(salt, 0, hashedPassWithSalt, 0, salt.Length);
+                    Buffer.BlockCopy(hashedBytes, 0, hashedPassWithSalt, salt.Length, hashedBytes.Length);
+
+                    return Convert.ToBase64String(hashedPassWithSalt);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return "";
         }
     }
 }
