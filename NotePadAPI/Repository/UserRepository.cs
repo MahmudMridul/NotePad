6ab1a80731b779d8e30ab8d2504534d3b73d9c85
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-08-30 09:10:00 +0600

    Add EmailExists function

--- NotePadAPI/Repository/UserRepository.cs
+++ NotePadAPI/Repository/UserRepository.cs
@@ -1,12 +1,21 @@
-﻿using NotePadAPI.Repository.IRepository;
+﻿using Microsoft.EntityFrameworkCore;
+using NotePadAPI.Db.IDb;
+using NotePadAPI.Repository.IRepository;
 
 namespace NotePadAPI.Repository
 {
     public class UserRepository : IUserRepository
     {
-        public Task<bool> EmailExists(string email)
+        private readonly IDbContext _db;
+
+        public UserRepository(IDbContext db)
         {
-            
+            _db = db;
+        }
+
+        public async Task<bool> EmailExists(string email)
+        {
+            return await _db.Users.AnyAsync(user => user.Email == email);   
         }
     }
 }
