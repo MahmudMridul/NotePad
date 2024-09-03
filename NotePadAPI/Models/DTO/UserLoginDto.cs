7ee37a892d0a813ee4587986f6dfd9b6e2d88de3
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-09-03 15:05:38 +0600

    Add UserLoginDto

---
+++ NotePadAPI/Models/DTO/UserLoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace NotePadAPI.Models.DTO
+{
+    public class UserLoginDto
+    {
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
