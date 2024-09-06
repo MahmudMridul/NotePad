ba510db8277856f784fc2a47522a0a0dadafdd20
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-09-06 10:06:24 +0600

    Remove App.js

--- notepad-ui/src/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import LoginRegistration from './pages/LoginRegistration'
-
-export default function App() {
-   return (
-      <div> <LoginRegistration/> </div>
-   )
-}
