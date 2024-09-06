567b96a068130814fddd7cdc5b75e968860a33d7
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-09-06 09:59:34 +0600

    Add Home

---
+++ notepad-ui/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material";
+import React from "react";
+import AppBar from "../components/AppBar";
+
+export default function Home() {
+   return (
+      <Box>
+         <AppBar />
+      </Box>
+   );
+}
