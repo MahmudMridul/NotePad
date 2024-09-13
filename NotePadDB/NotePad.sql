091f02d2c0835ead861b893479a44e2b5ebe3096
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-09-13 15:57:45 +0600

    Add insert notes query, get notes for users query

--- NotePadDB/NotePad.sql
+++ NotePadDB/NotePad.sql
@@ -6,3 +6,25 @@ from Users;
 
 select * 
 from Notes;
+
+insert into NotePad.dbo.Notes
+(Title, Description, CreatedAt, LastUpdatedAt, UserId) values 
+('First note', 'First note description', '2024-09-13', '2024-09-13', 1),
+('Second note', 'Second note description', '2024-09-13', '2024-09-13', 1),
+('Third note', 'Third note description', '2024-09-13', '2024-09-13', 1),
+('Fourth note', 'Fourth note description', '2024-09-13', '2024-09-13', 1),
+('First note two', 'First note description', '2024-09-13', '2024-09-13', 2),
+('Second note two', 'Second note description', '2024-09-13', '2024-09-13', 2),
+('Third note two', 'Third note description', '2024-09-13', '2024-09-13', 2);
+
+
+select 
+	u.Id,
+	u.Name,
+	u.Email,
+	n.Id,
+	n.Title, 
+	n.Description
+from Users u
+inner join Notes n
+on u.Id = n.UserId;
