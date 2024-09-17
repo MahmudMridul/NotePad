766be358e7cdc8ae423dda9f52552b1209918287
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-09-17 12:25:42 +0600

    Add common utility class

---
+++ NotePadAPI/Utils/Utility.cs
@@ -0,0 +1,18 @@
+﻿using NotePadAPI.Models;
+using System.Net;
+
+namespace NotePadAPI.Utils
+{
+    public class Utility
+    {
+        internal static ApiResponse CreateResponse(string msg, HttpStatusCode code, object? data = null, bool isSuccess = false)
+        {
+            ApiResponse response = new ApiResponse();
+            response.Message = msg;
+            response.StatusCode = code;
+            response.Data = data;
+            response.IsSuccess = isSuccess;
+            return response;
+        }
+    }
+}
