09fee4085ac8596a2028ce41d6be3862d2dc6def
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-09-20 08:39:43 +0600

    Modify CreateResponse

--- NotePadAPI/Utils/Utility.cs
+++ NotePadAPI/Utils/Utility.cs
@@ -7,12 +7,13 @@ namespace NotePadAPI.Utils
     {
         internal static ApiResponse CreateResponse(string msg, HttpStatusCode code, object? data = null, bool isSuccess = false)
         {
-            ApiResponse response = new ApiResponse();
-            response.Message = msg;
-            response.StatusCode = code;
-            response.Data = data;
-            response.IsSuccess = isSuccess;
-            return response;
+            return new ApiResponse
+            {
+                Message = msg,
+                StatusCode = code,
+                Data = data,
+                IsSuccess = isSuccess
+            };
         }
     }
 }
