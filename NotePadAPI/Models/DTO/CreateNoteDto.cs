f5ccb21b8f9ff02b88bcd29b04799d2a9e983f59
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-09-26 09:04:08 +0600

    Add CreateNoteDto

---
+++ NotePadAPI/Models/DTO/CreateNoteDto.cs
@@ -0,0 +1,9 @@
+﻿namespace NotePadAPI.Models.DTO
+{
+    public class CreateNoteDto
+    {
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public int UserId { get; set; }
+    }
+}
