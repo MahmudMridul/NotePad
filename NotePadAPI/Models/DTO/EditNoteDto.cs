abde124e4e68faf79996e7387a86212e0e765808
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-09-27 08:04:24 +0600

    Add EditNoteDto

---
+++ NotePadAPI/Models/DTO/EditNoteDto.cs
@@ -0,0 +1,8 @@
+﻿namespace NotePadAPI.Models.DTO
+{
+    public class EditNoteDto
+    {
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+    }
+}
