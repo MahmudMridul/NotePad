94ec667cc3c0ae4a72a1fa18c9e3b208a2458a92
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-10-29 15:33:04 +0600

    chore: remove unused imports

--- notepad-ui/src/appSlice.js
+++ notepad-ui/src/appSlice.js
@@ -1,6 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { apis } from "./utils";
-import { accordionActionsClasses } from "@mui/material";
 
 const initialState = {
    //FLAGS
